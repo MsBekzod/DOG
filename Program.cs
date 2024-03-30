dd18650dc74675561bec558e59df639d8034bcdf
MsBekzod <bekzodmuzaffarovich@example.com>
2024-03-30 21:51:11 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+string name = "Ismingizni kiriting:";
+System.Console.Write(name);
+string ismi = Console.ReadLine();
+string greeting = $"Assalom alekum  {ismi} aka";
+System.Console.WriteLine(greeting);
+
+System.Console.Write("Yoshingizni kiriting:");
+string age = Console.ReadLine();
+int yosh = Convert.ToInt32(age);
+int ageDifference = 2024 - yosh;
+System.Console.WriteLine($"Sizning tug'ulgan yilingiz {ageDifference}-yil!");
+
+
